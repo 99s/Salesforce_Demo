df684f0239c433f782cc58ea83b0396a36570a17
99s <divi9e@gmail.com>
2022-01-04 00:18:13 +0530

    searchbox - book app

--- bookdemoapp/force-app/main/default/lwc/bookApp/bookApp.js
+++ bookdemoapp/force-app/main/default/lwc/bookApp/bookApp.js
@@ -3,6 +3,7 @@ const BOOK_URL = 'https://www.googleapis.com/books/v1/volumes?q=';
 export default class BookApp extends LightningElement {
     searchKey='spiderman';
     books;
+    timer;
     connectedCallback(){
         this.fetchGoogleBookApiData(BOOK_URL);
     }
@@ -19,4 +20,14 @@ export default class BookApp extends LightningElement {
          this.books = data
         })
      .catch(error=>console.error(error))}
+
+     fetchBookHandler(event){
+         this.searchKey = event.target.value;
+         window.clearTimeout(this.timer);
+         this.timer = setTimeout(()=>{
+             this.fetchGoogleBookApiData(BOOK_URL)
+         }, 1000
+         )
+
+     }
 }
