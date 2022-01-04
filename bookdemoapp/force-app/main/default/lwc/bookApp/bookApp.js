2d11ff8500e08d97ce0f05edd4c03b03fe704ad1
99s <divi9e@gmail.com>
2022-01-04 16:38:09 +0530

    buy button working

--- bookdemoapp/force-app/main/default/lwc/bookApp/bookApp.js
+++ bookdemoapp/force-app/main/default/lwc/bookApp/bookApp.js
@@ -1,12 +1,46 @@
 import { LightningElement } from 'lwc';
+import makeGetcallout from '@salesforce/apex/StripeCallout.makeGetcallout';
 const BOOK_URL = 'https://www.googleapis.com/books/v1/volumes?q=';
 export default class BookApp extends LightningElement {
     searchKey='spiderman';
     books;
     timer;
+    isForSale = false;
     connectedCallback(){
         this.fetchGoogleBookApiData(BOOK_URL);
     }
+   
+   
+
+    handleBuyBook() {
+        console.log('--------- handleBuyBook --------------');
+        makeGetcallout()
+            .then(result => {
+                console.log(result);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+    
+    checkBookSaleability(bookdata){
+        if(bookdata != null){
+            //console.log('---------book Length --------------');
+            //console.log(Object.keys(bookdata).length);
+            //console.log(bookdata.length);
+           bookdata.items.forEach((bookitem)=>{
+               if(bookitem.saleInfo.saleability == 'FOR_SALE'){
+                   bookitem.saleInfo.saleabilitybool = true;
+               }
+               else{
+                   bookitem.saleInfo.saleabilitybool = false;
+               }
+           })
+        }
+        return bookdata;
+    }
+
+
     fetchGoogleBookApiData(url){fetch(url+this.searchKey
     //     ,
     //      {
@@ -16,8 +50,12 @@ export default class BookApp extends LightningElement {
     //  }
      ).then(response=>response.json())
      .then(data=>{
-         console.log(data)
-         this.books = data
+         console.log(data);
+         console.log('---------book inint Length --------------');
+         console.log(data.items.length);
+         this.books = this.checkBookSaleability(data);
+         console.log('------UPDATED DATA-------');
+         console.log(this.books);
         })
      .catch(error=>console.error(error))}
 
@@ -30,4 +68,7 @@ export default class BookApp extends LightningElement {
          )
 
      }
+
+     //check if book is for sale 
+     
 }
